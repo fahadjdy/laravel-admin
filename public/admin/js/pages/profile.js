9bc2b16a44553e3f0be5b8a7eba93c676cde4ae4
Fahad Jadiya <fahadjdy12@gmail.com>
2025-02-12 13:04:52 -0800

    profile page changes

--- public/admin/js/pages/profile.js
+++ public/admin/js/pages/profile.js
@@ -1,3 +1,6 @@
+
+// Profile Page Js
+
 let profileIcon = document.getElementById('profile-icon');
 let profileFile = document.getElementById('profile-file');
 let profileImg = document.getElementById('profile-img');
@@ -17,6 +20,20 @@ profileFile.addEventListener('change', function() {
 });
 
 
+
+// Bio Data Js 
+
+ClassicEditor
+.create(document.querySelector('#address'), {
+    height: 300  // Optional: Set the height for the editor
+})
+.catch(error => {
+    console.error(error);
+});
+
+
+// Site Detail Js
+
 let faviconIcon = document.getElementById('favicon-icon');
 let faviconFile = document.getElementById('favicon-file');
 let faviconImg = document.getElementById('favicon-img');
@@ -35,33 +52,3 @@ faviconFile.addEventListener('change', function() {
 });
 
 
-$(document).ready(function(){
-    $('#edit-btn').on('click', function(){
-        $('#edit-btn').hide();
-        $('#save-btn').show();
-        $('#name-text').addClass('d-none');
-        $('#name').removeClass('d-none');
-        $('#slogan-text').addClass('d-none');
-        $('#slogan').removeClass('d-none');
-        $('#email-text').addClass('d-none');
-        $('#email').removeClass('d-none');
-        $('#contact-text').addClass('d-none');
-        $('#contact').removeClass('d-none');
-        $('#address-text').addClass('d-none');
-        $('#address').removeClass('d-none');
-    });
-    $('#save-btn').on('click', function(){
-        $('#edit-btn').show();
-        $('#save-btn').hide();
-        $('#name-text').removeClass('d-none');
-        $('#name').addClass('d-none');
-        $('#slogan-text').removeClass('d-none');
-        $('#slogan').addClass('d-none');
-        $('#email-text').removeClass('d-none');
-        $('#email').addClass('d-none');
-        $('#contact-text').removeClass('d-none');
-        $('#contact').addClass('d-none');
-        $('#address-text').removeClass('d-none');
-        $('#address').addClass('d-none');
-    });
-});
---
+++ resources/views/admin/view/profile/site-details.blade.php
@@ -0,0 +1,16 @@
+<div class="form-container rounded">
+    <tr>
+        <td width="10%" class="text-primary" >Fevicon :</td>
+        <td>
+            <div class="position-relative">
+                <img src="{{ asset('author/fhd-favicon.png') }}" width="50px" height="50px" id="favicon-img" >
+                <div class="favicon-img-overlay position-absolute ">
+                    <div class="circle">
+                        <i class="fa-duotone fa-solid fa-camera" id="favicon-icon"></i>
+                        <input type="file" class="d-none" id="favicon-file" name="favicon" accept="image/*">
+                    </div>
+                </div>
+            </div>
+        </td>
+    </tr>
+</div>
