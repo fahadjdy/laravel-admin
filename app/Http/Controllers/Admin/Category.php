899df9bb0d0f31c6ceccc0cfd14b73da99bdc2ed
Fahad Jadiya <fahadjdy12@gmail.com>
2025-02-24 09:05:28 -0800

    watermark issue solved

--- app/Http/Controllers/Admin/Category.php
+++ app/Http/Controllers/Admin/Category.php
@@ -225,7 +225,7 @@ class Category extends Controller
 
                 // Apply watermark if enabled
                 if ($is_watermark && file_exists($watermark)) {
-                    applyWatermark($imagePath, $watermark, $imagePath, 'center', 10, 10,75);
+                    applyTextWatermark($imagePath, $imagePath, "Stech Quality Parts", 15);
                 }
 
                 // Save image path in category_images table
