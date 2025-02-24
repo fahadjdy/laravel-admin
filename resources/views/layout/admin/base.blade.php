d9e352301dfa62160c24bf202247da93da35692c
fahad.jadiya <fahad.jadiya@techaxion.com>
2025-02-24 11:39:53 +0530

    sidebar saprated

--- resources/views/layout/admin/base.blade.php
+++ resources/views/layout/admin/base.blade.php
@@ -21,28 +21,7 @@
     
     <div class="container-fluid">
         <aside class="sidebar">
-            <ul>
-                <li>
-                    <a href="{{ url('/admin/dashboard') }}" class="active"> <i class="fa-duotone fa-solid fa-home"></i> Dashboard</a>
-                </li>
-                <li>
-                    <a href="{{ url('/admin/category') }}"> <i class="fa-duotone fa-solid fa-user"></i> Category</a>
-                </li>
-                <li>
-                    <a href="{{ url('/admin/slider') }}"> <i class="fa-duotone fa-solid fa-image"></i> Slider</a>
-                </li>
-                <li>
-                    <a href="javascript:void(0);" class="toggle-gallery">
-                        <span><i class="fa-duotone fa-solid fa-image"></i> Gallery</span>
-                        <i class="fa-duotone fa-solid fa-angle-down toggle-icon" style="float: right;"></i>
-                    </a>
-                    <ul class="gallery-submenu" style="display: none; padding-left: 20px;">
-                        <li><a href="{{ url('/admin/slider') }}">Image Gallery</a></li>
-                        <li><a href="{{ url('/admin/slider') }}">Video Gallery</a></li>
-                    </ul>
-                </li>
-            </ul>
-
+            @include('layout.admin.sidebar')
         </aside>
         <main>
             @yield('content')
